4d2d202495b68183a04194e5d4cd6bcc6070faa7
Juju Adams <julian.adams@email.com>
2023-05-02 19:34:43 +0100

    Adds 4.1 docs

--- config.js
+++ config.js
@@ -22,8 +22,8 @@
 const config = {
 	name: 'Snitch',
 	description: 'Logging and crash handling system for GameMaker Studio 2022 LTS',
-	latestVersion: '4.0',
-	otherVersions: ['3.1'],
+	latestVersion: '4.1',
+	otherVersions: ['4.0', '3.1'],
 	favicon: 'assets/favicon.ico',
 	themeColor: '#88a3b8',
 };
